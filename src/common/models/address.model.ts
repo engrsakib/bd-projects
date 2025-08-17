a31f7f1154fe115a366201dd0aadbff025db437e
engrsakib <snazmussakib01@gmail.com>
2025-08-17 23:31:56 +0600

    location update API done

--- src/common/models/address.model.ts
+++ src/common/models/address.model.ts
@@ -1,14 +1,17 @@
 import { IAddress } from "@/interfaces/common.interface";
 import { Schema } from "mongoose";
 
-export const addressSchema = new Schema<IAddress>({
-  division: { type: String, default: "" },
-  district: { type: String, default: "" },
-  thana: { type: String, default: "" },
-  zip_code: { type: String, default: "" },
-  local_address: { type: String, default: "" },
-  coordinates: {
-    latitude: { type: Number, default: null },
-    longitude: { type: Number, default: null },
+export const addressSchema = new Schema<IAddress>(
+  {
+    division: { type: String, default: "" },
+    district: { type: String, default: "" },
+    thana: { type: String, default: "" },
+    zip_code: { type: String, default: "" },
+    local_address: { type: String, default: "" },
+    coordinates: {
+      latitude: { type: Number, default: null },
+      longitude: { type: Number, default: null },
+    },
   },
-});
+  { _id: false }
+);
