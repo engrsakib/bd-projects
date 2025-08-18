b8581b90786478758d3f62b6b4b192b0b84727c8
engrsakib <snazmussakib01@gmail.com>
2025-08-18 22:37:43 +0600

    product create route define done

---
+++ src/modules/product/product.routes.ts
@@ -0,0 +1,18 @@
+import { Router } from "express";
+import { ProductController } from "./product.controller";
+import { upload } from "@/config/multer";
+import { ProductMiddleware } from "@/middlewares/upload.product.middleware";
+
+const router = Router();
+
+router.post(
+  "/",
+  upload.fields([
+    { name: "thumbnail", maxCount: 1 },
+    { name: "slider_images", maxCount: 5 },
+  ]),
+  ProductMiddleware.uploadImages,
+  ProductController.create
+);
+
+export const ProductRoutes = router;
