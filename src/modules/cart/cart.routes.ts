30170c0a68dfbbe16bf1e5e6bc6e7709ff0dae32
engrsakib <snazmussakib01@gmail.com>
2025-08-29 19:27:46 +0600

    add to cart zod validation schema design and implementation done

--- src/modules/cart/cart.routes.ts
+++ src/modules/cart/cart.routes.ts
@@ -2,12 +2,15 @@ import { Router } from "express";
 import { CartController } from "./cart.controller";
 import { JwtInstance } from "@/lib/jwt";
 import { ROLES } from "@/constants/roles";
+import validateRequest from "@/middlewares/validateRequest";
+import { cartValidations } from "./cart.validate";
 
 const router = Router();
 
 router.post(
   "/",
   JwtInstance.authenticate([ROLES.CUSTOMER]),
+  validateRequest(cartValidations.addToCart),
   CartController.addToCart
 );
 
