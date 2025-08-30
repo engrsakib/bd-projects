0271ca438556e4592fa3c5d8d8b8b517c33543c9
engrsakib <snazmussakib01@gmail.com>
2025-08-30 12:39:14 +0600

    banner file upload middleware setup completed && create API testing done

--- src/modules/banner/banner.routes.ts
+++ src/modules/banner/banner.routes.ts
@@ -1,8 +1,15 @@
 import { Router } from "express";
 import { BannerController } from "./banner.controller";
+import { upload } from "@/config/multer";
+import { BannerMiddleware } from "@/middlewares/upload.banner.middleware";
 
 const router = Router();
 
-router.post("/", BannerController.create);
+router.post(
+  "/",
+  upload.single("thumbnail"),
+  BannerMiddleware.uploadBanner,
+  BannerController.create
+);
 
 export const BannerRoutes = router;
