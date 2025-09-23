b32fc70f98ec7b8936c45556d46e5050e9705a5c
engrsakib <snazmussakib01@gmail.com>
2025-09-24 02:08:02 +0600

    courier service init

--- src/modules/courier/courier.controller.ts
+++ src/modules/courier/courier.controller.ts
@@ -1,5 +1,30 @@
 import BaseController from "@/shared/baseController";
+import { Request, Response } from "express";
+import HttpStatus from "http-status";
+import { CourierService } from "./courier.service";
 
-class Controller extends BaseController {}
+class Controller extends BaseController {
+  transferToCourier = this.catchAsync(async (req: Request, res: Response) => {
+    const { order_id } = req.params;
+    const { note } = req.body;
+    if (!order_id) {
+      return this.sendResponse(res, {
+        statusCode: HttpStatus.BAD_REQUEST,
+        success: false,
+        message: "Order ID is required",
+      });
+    }
+
+    const result = await CourierService.transferToCourier(order_id, {
+      note: note || "Order transferred to courier",
+    });
+    return this.sendResponse(res, {
+      statusCode: HttpStatus.OK,
+      success: true,
+      message: "Order transferred to courier successfully",
+      data: result,
+    });
+  });
+}
 
 export const CourierController = new Controller();
