2b3321c6a41ff7a146ad6266a9e30803758dffbd
engrsakib <snazmussakib01@gmail.com>
2025-09-25 17:17:53 +0600

    cms routes init

---
+++ src/modules/cms/cms.router.ts
@@ -0,0 +1,49 @@
+import express from "express";
+import { ContentManagementController } from "./cms.controller";
+import { JwtInstance } from "@/lib/jwt";
+import { ROLES } from "@/constants/roles";
+
+const router = express.Router();
+
+// Public routes
+router.get("/banners", ContentManagementController.getBanners);
+
+// Admin routes
+router.patch(
+  "/add/banner",
+  JwtInstance.authenticate([ROLES.ADMIN, ROLES.SUPER_ADMIN]),
+  ContentManagementController.addBanner
+);
+router.patch(
+  "/add/banner/:bannerId/:productId",
+  JwtInstance.authenticate([ROLES.ADMIN, ROLES.SUPER_ADMIN]),
+  ContentManagementController.addProductIdToBanner
+);
+router.patch(
+  "/delete/banner/:bannerId/:productId",
+  JwtInstance.authenticate([ROLES.ADMIN, ROLES.SUPER_ADMIN]),
+  ContentManagementController.deleteProductIdToBanner
+);
+router.delete(
+  "/delete-banner/:bannerId",
+  JwtInstance.authenticate([ROLES.ADMIN, ROLES.SUPER_ADMIN]),
+  ContentManagementController.deleteBanner
+);
+
+router.get(
+  "/featured-products",
+  ContentManagementController.getFeaturedProducts
+);
+
+router.post(
+  "/admin/featured-products",
+  JwtInstance.authenticate([ROLES.ADMIN, ROLES.SUPER_ADMIN]),
+  ContentManagementController.addFeaturedProducts
+);
+router.delete(
+  "/admin/featured-products/:variant_id",
+  JwtInstance.authenticate([ROLES.ADMIN, ROLES.SUPER_ADMIN]),
+  ContentManagementController.deleteFeaturedProducts
+);
+
+export const ContentManagementRoutes = router;
