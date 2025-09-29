12fdb0e42c6a934720a9880ce226579e94d6c390
engrsakib <snazmussakib01@gmail.com>
2025-09-29 12:37:38 +0600

    permission routes init

---
+++ src/modules/permission/permission.route.ts
@@ -0,0 +1,9 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
+import { Router } from "express";
+
+import { JwtInstance } from "@/lib/jwt";
+import { ROLES } from "@/constants/roles";
+
+const router = Router();
+
+export const PermissionsRoutes = router;
