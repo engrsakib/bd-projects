4ae1adc65bce57db43ec750f2fba0b1d7a419e30
engrsakib <snazmussakib01@gmail.com>
2025-09-30 17:50:45 +0600

    order logs ready

--- src/modules/order/order.controller.ts
+++ src/modules/order/order.controller.ts
@@ -53,10 +53,19 @@ class Controller extends BaseController {
   });
 
   updateOrderStatus = this.catchAsync(async (req: Request, res: Response) => {
+    const { id, status } = req.body;
+    if (!id || !status) {
+      return this.sendResponse(res, {
+        statusCode: HttpStatusCode.BAD_REQUEST,
+        success: false,
+        message: "Order ID and Status are required",
+      });
+    }
+
     const data = await OrderService.updateOrderStatus(
-      req.body.id as string,
+      id as string,
       req.user?.id as string,
-      req.body.status
+      status
     );
     this.sendResponse(res, {
       statusCode: HttpStatusCode.OK,
