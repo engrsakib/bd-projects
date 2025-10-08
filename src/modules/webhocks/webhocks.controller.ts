fbab9635ed2e200d5d48a42b73c74add12ab928a
engrsakib <snazmussakib01@gmail.com>
2025-10-08 11:36:28 +0600

    steadfastwebhocks service initialize

--- src/modules/webhocks/webhocks.controller.ts
+++ src/modules/webhocks/webhocks.controller.ts
@@ -1,10 +1,11 @@
 import BaseController from "@/shared/baseController";
 import { Request, Response } from "express";
 import { HttpStatusCode } from "@/lib/httpStatus";
+import { WebhocksService } from "./webhocks.service";
 
 class Controller extends BaseController {
   steadfastWebhock = this.catchAsync(async (req: Request, res: Response) => {
-    const result = "Steadfast webhock executed";
+    const result = await WebhocksService.steadfastWebhock(req.body);
     this.sendResponse(res, {
       statusCode: HttpStatusCode.OK,
       success: true,
