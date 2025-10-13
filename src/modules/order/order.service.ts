48f407b39ea605bf08f12c0464d9bf952c0332fd
engrsakib <snazmussakib01@gmail.com>
2025-10-13 10:31:23 +0600

    stock total solds added for order create by user

--- src/modules/order/order.service.ts
+++ src/modules/order/order.service.ts
@@ -74,6 +74,7 @@ class Service {
         // console.log(consumedLots, "consumed lots `");
 
         stock.available_quantity -= item.quantity;
+        stock.total_sold = (stock.total_sold || 0) + item.quantity;
         await stock.save({ session });
       }
 
