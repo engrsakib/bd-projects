370bf32af97da8c281b604dc35b3b973dd5727d8
engrsakib <snazmussakib01@gmail.com>
2025-11-03 19:46:54 +0600

    order update

--- src/modules/order/order.controller.ts
+++ src/modules/order/order.controller.ts
@@ -121,6 +121,34 @@ class Controller extends BaseController {
     });
   });
 
+  updateOrderStatusBulk = this.catchAsync(
+    async (req: Request, res: Response) => {
+      const { ids, status } = req.body;
+      if (!ids || !status) {
+        return this.sendResponse(res, {
+          statusCode: HttpStatusCode.BAD_REQUEST,
+          success: false,
+          message: "Order IDs and Status are required",
+        });
+      }
+
+      const concurrency = Math.min(ids.length, 5);
+      const params = {
+        ids,
+        userId: req.user?.id as string,
+        status,
+        concurrency,
+      };
+      const data = await OrderService.updateOrdersStatusBulk(params);
+      this.sendResponse(res, {
+        statusCode: HttpStatusCode.OK,
+        success: true,
+        message: "Order status updated successfully",
+        data,
+      });
+    }
+  );
+
   deleteOrder = this.catchAsync(async (req: Request, res: Response) => {
     if (!req.params.id) {
       return this.sendResponse(res, {
