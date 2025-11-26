008270210152d2f3d37ace75c8022119d68f8190
engrsakib <snazmussakib01@gmail.com>
2025-11-26 15:42:45 +0600

    data and data set update data change data add

--- src/modules/barcode/barcode.controller.ts
+++ src/modules/barcode/barcode.controller.ts
@@ -146,6 +146,27 @@ class Controller extends BaseController {
       data: result,
     });
   });
+
+  checkBarcodeUsedOrNot = this.catchAsync(
+    async (req: Request, res: Response) => {
+      const barcode = req.params.barcode;
+      if (!barcode) {
+        this.sendResponse(res, {
+          statusCode: 400,
+          success: false,
+          message: "Barcode is required",
+        });
+        return;
+      }
+      const result = await UniqueBarcodeService.checkBarcodeUsedOrNot(barcode);
+      this.sendResponse(res, {
+        statusCode: 200,
+        success: true,
+        message: "Barcode usage status retrieved successfully",
+        data: result,
+      });
+    }
+  );
 }
 
 export const UniqueBarcodeController = new Controller();
