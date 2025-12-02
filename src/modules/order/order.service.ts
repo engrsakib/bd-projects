2e602a829fafe86f9ae51fba3cc2f70711031872
engrsakib <snazmussakib01@gmail.com>
2025-12-02 10:02:45 +0600

    lot count zero

--- src/modules/order/order.service.ts
+++ src/modules/order/order.service.ts
@@ -84,7 +84,10 @@ class Service {
         // console.log(stock.available_quantity, "available qnt");
         // console.log(item.quantity, "item qnt");
 
-        if (stock.available_quantity < item.quantity) {
+        if (
+          Math.abs(stock.qty_reserved - stock.available_quantity) <
+          item.quantity
+        ) {
           total_stock_issue = true;
           item.status = ORDER_STATUS.AWAITING_STOCK;
           continue;
@@ -100,7 +103,7 @@ class Service {
           item.lots = consumedLots;
           // console.log(consumedLots, "consumed lots `");
 
-          stock.available_quantity -= item.quantity;
+          stock.qty_reserved += item.quantity;
           stock.total_sold = (stock.total_sold || 0) + item.quantity;
           item.total_sold = (item.total_sold || 0) + item.quantity;
           await stock.save({ session });
@@ -2679,7 +2682,7 @@ class Service {
         lot.qty_available -= remaining;
         remaining = 0;
       }
-      await lot.save({ session });
+      // await lot.save({ session });
 
       consumption.push({
         lotId: lot._id.toString(),
